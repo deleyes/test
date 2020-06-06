465f00dcae7573b967bd4c77334a9e31cceb4041
deleyes <steven_deleye@hotmail.com>
2020-06-06 23:00:21 +0200

    first commit

---
+++ src/com/example/helloworld/TestClass.java
@@ -0,0 +1,13 @@
+package src.com.example.helloworld;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        float value1 = 102;
+        float value2 = (int)102.0;
+        float value3 = 1f * 0.0;
+        float value4 = 1f * (short)0.0;
+        float value5 = 1f * (boolean)0;
+    }
+}
